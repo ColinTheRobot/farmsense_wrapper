745b3dab3c48ad898b71443ced31148bf1b22e26
cohart <me@colin-hart.com>
2014-04-08 22:20:03 -0400

    added frostdates class

--- lib/farmsense_wrapper/frost_dates.rb
+++ lib/farmsense_wrapper/frost_dates.rb
@@ -1,1 +1,31 @@
-frost_dates.rb
+require 'pry'
+require 'httparty'
+require 'json'
+
+
+class FrostDates
+  attr_reader :endpoint_url
+  attr_writer :station_id, :season
+# this object gets initialized with a station id found in stations.rb and a season.
+# 1 for spring, 2 for fall.
+  def initialize(args)
+    @station_id = args[:station_id]
+    @season = args[:season]
+    @endpoint_url = "http://farmsense-prod.apigee.net/v1/frostdates/probabilities/?station=#{@station_id}&season=#{@season}"
+  end
+
+  def get
+    raw = HTTParty.get(endpoint_url)
+    @parsed_response = parse(raw)
+  end
+
+  def parse(data)
+    JSON.parse(data)
+  end
+
+  def display
+    @parsed_response
+  end
+end
+
+binding.pry
